d56f63cead26ac6ab85cf2f41ddab228350c1ee3
Anshifali <anshifali63709@gmail.com>
2026-01-29 12:12:54 +0530

    DB added

--- connection.js
+++ connection.js
@@ -1,7 +1,8 @@
 import mongoose from "mongoose";
-
+import dotenv from 'dotenv'
+dotenv.config()
 export default async function connection() {
-    const db = await mongoose.connect('mongodb://127.0.0.1:27017/Backend')
+    const db = await mongoose.connect(`${process.env.DB_URL}/Backend`)
     console.log("database connected");
     return db
     
